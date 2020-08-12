b853a65b4faabd83b9ac93c6869fec8f7fea6a6e
David Coats <david@getsafetycone.com>
2020-08-11 18:18:56 -0700

    Added ServiceTestFixtureBase type.

--- source/R5T.T0011/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.T0011
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.T0011/Code/Classes/Base Classes/ServiceTestFixtureBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.T0011
+{
+    public abstract class ServiceTestFixtureBase<TService>
+    {
+        protected TService Service { get; private set; }
+
+
+        public ServiceTestFixtureBase(IServiceProvider serviceProvider)
+        {
+            this.Service = serviceProvider.GetRequiredService<TService>();
+        }
+    }
+}
